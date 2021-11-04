8acea62e43c1c511f8092fc206be99f0e2c615dc
pAndressa <p.andressasilva@gmail.com>
2021-11-04 17:10:36 -0300

    Inclusao dos handlers

--- PaymentContext.Domain/Commands/CreatePayPalSubscriptionCommad.cs
+++ PaymentContext.Domain/Commands/CreatePayPalSubscriptionCommad.cs
@@ -9,7 +9,7 @@ namespace PaymentContext.Domain.Commands
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
-        public string Address { get; set; }
+        public string Email { get; set; }
 
         public string TransactionCode { get; set; }
         public string PaymentNumber { get; set; }
--- PaymentContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -1,7 +1,11 @@
+using System;
 using Flunt.Notifications;
 using Flunt.Validations;
 using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
 using PaymentContext.Domain.Repositories;
+using PaymentContext.Domain.Services;
+using PaymentContext.Domain.ValueObjects;
 using PaymentContext.Shared.Commands;
 using PaymentContext.Shared.Handlers;
 
@@ -9,13 +13,16 @@ namespace PaymentContext.Domain.Handlers
 {
     public class SubscriptionHandler :
                 Notifiable,
-                IHandler<CreateBoletoSubscriptionCommand>
+                IHandler<CreateBoletoSubscriptionCommand>,
+                IHandler<CreatePayPalSubscriptionCommand>
     {
         private readonly IStudentRepository _repository;
+        private readonly IEmailService _emailService;
 
-        public SubscriptionHandler(IStudentRepository repository)
+        public SubscriptionHandler(IStudentRepository repository, IEmailService emailService)
         {
             _repository = repository;
+            _emailService = emailService;
         }
         public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
         {
@@ -28,8 +35,96 @@ namespace PaymentContext.Domain.Handlers
                 return new CommandResult(false, "Não foi possível realizar sua assinatura");
             }
 
-            AddNotifications(new Contract());
+            if(_repository.DocumentExists(command.Document))
+                AddNotification("Document","Este CPF já está em uso");
 
+            if(_repository.EmailExists(command.Email))
+                AddNotification("Email","Este e-mail já está em uso");
+
+            //Gerar value onjects
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, Enums.EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+
+            //gerar as entidades
+            var student = new Student(name, document,email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new BoletoPayment(
+                command.BarCode,
+                command.BoletoNumber,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                command.Payer,
+                new Document(command.PayerDocument , command.PayerDocumentType),
+                address,
+                email
+            );
+
+            //relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            //Agrupar as notificações
+            AddNotifications(name, document, email, student, subscription, payment);
+
+            //Salvar as informções
+            _repository.CreateSubscription(student);
+
+            // Enviar e-mail de boas vindas
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "Bem vindo","Sua assinatura foi criada");
+
+            
+            //retornar
+            return new CommandResult(true, "Assinatura cadastrada com sucesso!");
+        }
+
+        public ICommandResult Handle(CreatePayPalSubscriptionCommand command)
+        {
+            if(_repository.DocumentExists(command.Document))
+                AddNotification("Document","Este CPF já está em uso");
+
+            if(_repository.EmailExists(command.Email))
+                AddNotification("Email","Este e-mail já está em uso");
+
+            //Gerar value onjects
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, Enums.EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+
+            //Gerar as entidades
+            var student = new Student(name, document,email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new PayPalPayment(
+                command.TransactionCode,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                command.Payer,
+                new Document(command.PayerDocument , command.PayerDocumentType),
+                address,
+                email
+            );
+
+            //relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            //Agrupar as notificações
+            AddNotifications(name, document, email, student, subscription, payment);
+
+            //Salvar as informções
+            _repository.CreateSubscription(student);
+
+            // Enviar e-mail de boas vindas
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "Bem vindo","Sua assinatura foi criada");
+
+            
+            //retornar
             return new CommandResult(true, "Assinatura cadastrada com sucesso!");
         }
     }
